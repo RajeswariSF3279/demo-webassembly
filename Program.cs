32e19dea1ca44b38e12db3697d3341b14d15f581
kmuthukumarmkm <muthukumar.kannan@syncfusion.com>
2022-02-16 20:51:56 +0530

    Adding Samples

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using demo_webassembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
 
 await builder.Build().RunAsync();
